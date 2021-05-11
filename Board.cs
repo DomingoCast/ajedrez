e41f677667dfff9cfac236f7407d73f184666b55
Domingo Castillo <castillomartidomingo@gmail.com>
2021-05-11 09:44:59 +0200

    color pieces array

--- Board.cs
+++ Board.cs
@@ -2,38 +2,81 @@ using System;
 public class Board
 {
     private Piece[,] map;
+    private Piece[] whitePieces;
+    private Piece[] blackPieces;
     public Board()
     {
         map = new Piece[8,8];
+        whitePieces = new Piece[16];
+        blackPieces = new Piece[16];
+
+        createPieces();
 
         fillBoard();
 
     }
 
+    private void createPieces()
+    {
+        whitePieces[0] = new Rook("white");
+        whitePieces[1] = new Knight("white");
+        whitePieces[2] = new Bishop("white");
+        whitePieces[3] = new Queen("white");
+        whitePieces[4] = new King("white");
+        whitePieces[5] = new Bishop("white");
+        whitePieces[6] = new Knight("white");
+        whitePieces[7] = new Rook("white");
+        
+        blackPieces[0] = new Rook("black");
+        blackPieces[1] = new Knight("black");
+        blackPieces[2] = new Bishop("black");
+        blackPieces[3] = new Queen("black");
+        blackPieces[4] = new King("black");
+        blackPieces[5] = new Bishop("black");
+        blackPieces[6] = new Knight("black");
+        blackPieces[7] = new Rook("black");
+
+        for(int j = 8; j<16; j++)
+        {
+            whitePieces[j] = new Pawn("white");
+            blackPieces[j] = new Pawn("black");
+        }
+
+    }
     private void fillBoard()
     {
         for(int j = 0; j<8; j++)
         {
-            map[1,j] = new Pawn("white");
-            map[6,j] = new Pawn("black");
+            map[0,j] = whitePieces[j];
+            map[7,j] = blackPieces[j];
         }
-        map[0,0] = new Rook("white");
-        map[0,1] = new Knight("white");
-        map[0,2] = new Bishop("white");
-        map[0,3] = new Queen("white");
-        map[0,4] = new King("white");
-        map[0,5] = new Bishop("white");
-        map[0,6] = new Knight("white");
-        map[0,7] = new Rook("white");
+        for(int j = 0; j<8; j++)
+        {
+            map[1,j] = whitePieces[j+8];
+            map[6,j] = blackPieces[j+8];
+        }
+        //map[0,0] = new Rook("white");
+        //map[0,1] = new Knight("white");
+        //map[0,2] = new Bishop("white");
+        //map[0,3] = new Queen("white");
+        //map[0,4] = new King("white");
+        //map[0,5] = new Bishop("white");
+        //map[0,6] = new Knight("white");
+        //map[0,7] = new Rook("white");
         
-        map[7,0] = new Rook("black");
-        map[7,1] = new Knight("black");
-        map[7,2] = new Bishop("black");
-        map[7,3] = new Queen("black");
-        map[7,4] = new King("black");
-        map[7,5] = new Bishop("black");
-        map[7,6] = new Knight("black");
-        map[7,7] = new Rook("black");
+        //map[7,0] = new Rook("black");
+        //map[7,1] = new Knight("black");
+        //map[7,2] = new Bishop("black");
+        //map[7,3] = new Queen("black");
+        //map[7,4] = new King("black");
+        //map[7,5] = new Bishop("black");
+        //map[7,6] = new Knight("black");
+        //map[7,7] = new Rook("black");
+    }
+
+    private void checkChecks()
+    {
+        
     }
 
     public bool Move(int[] pos1, int[] pos2, string color)

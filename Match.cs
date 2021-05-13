e7c6cfdfe14835a559ae70b0b3a2a73df1bf1c1a
Domingo Castillo <castillomartidomingo@gmail.com>
2021-05-13 09:10:47 +0200

    kinda algebraic notation

--- Match.cs
+++ Match.cs
@@ -23,8 +23,9 @@ public class Match
             string[] move = Console.ReadLine().Split(" ");
             try
             {
-                int[] pos1 = {Convert.ToInt32(move[0].Split(",")[1]) - 1, Convert.ToInt32(move[0].Split(",")[0]) - 1};
-                int[] pos2 = {Convert.ToInt32(move[1].Split(",")[1]) - 1, Convert.ToInt32(move[1].Split(",")[0]) - 1};
+                int[] pos1 = {Convert.ToInt32(move[0].Substring(1)) - 1, Convert.ToInt32(Convert.ToChar(move[0].Substring(0,1)) - 'a')};
+                int[] pos2 = {Convert.ToInt32(move[1].Substring(1)) - 1, Convert.ToInt32(Convert.ToChar(move[1].Substring(0,1)) - 'a')};
+                //int[] pos2 = {Convert.ToInt32(move[1].Split(",")[1]) - 1, Convert.ToInt32(Convert.ToChar(move[1].Split(",")[0]) - 'a'))};
                 error = !board.Move(pos1, pos2, turn);
                 if(!error)
                 {
